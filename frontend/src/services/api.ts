5f0eb5ecf67410c8509d3210c72f32eb97e6f09f
willy7890 <magasostephenchitasa@gmail.com>
2026-07-14 01:47:39 +0300

    API correction

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "/api/v1";
+// const API_URL = import.meta.env.VITE_API_URL || "/api/v1";
+const API_URL = import.meta.env.VITE_API_URL || "https://dream-together-backend-cgqz.onrender.com/api/v1";
 
 export const api = axios.create({ baseURL: API_URL });
 
